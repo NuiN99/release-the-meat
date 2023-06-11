45f372c35cd0d95183d65d5740e8b77e9e5a4e1d
M-Quinn <47506506+M-Quinn@users.noreply.github.com>
2023-06-11 11:59:42 -0400

    notes on PartButtons

--- ReleaseTheMeat/Assets/_Scripts/PartControllers/PartButtons.cs
+++ ReleaseTheMeat/Assets/_Scripts/PartControllers/PartButtons.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+//Remove unused statements
 
+//namespace
 public class PartButtons : MonoBehaviour
 {
+    //This doesn't need to be a singleton
+    //Singleton needs to be setup for MonoBehaviour
     public static PartButtons instance;
 
     public PartSelection.PartType selectedPartType;
@@ -16,6 +20,7 @@ public class PartButtons : MonoBehaviour
             instance = this;
     }
 
+    //Does this need to be a MonoBehaviour? if you keep this a singleton then this shouldn't be a monoBehaviour. otherwise leave it a MonoBehaviour and remove the singleton
     public void SelectPlank()
     {
         selectedPartType = PartSelection.PartType.PLANK;
