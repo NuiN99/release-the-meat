a2ed8af9c10ef5ca5b5c0a7c61c7c5328085418e
M-Quinn <47506506+M-Quinn@users.noreply.github.com>
2023-06-11 11:23:28 -0400

    Added comments to SceneManager

--- ReleaseTheMeat/Assets/_Scripts/GameControllers/SceneController.cs
+++ ReleaseTheMeat/Assets/_Scripts/GameControllers/SceneController.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+//You see these grey using statements? if you hover over them and then press [ctrl + .] then it will prompt you to have them removed
+//You don't want to include using statements for libraries that you won't use
+
+
+//Where is the namespace?
+//ALWAYS use a namespace
 
 public class SceneController : MonoBehaviour
 {
 
+    //If you aren't using the Start method then remove it. Unity calls it even if it's empty. It's not much but good practice
     void Start()
     {
         
